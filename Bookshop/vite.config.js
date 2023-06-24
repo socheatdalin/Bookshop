604c7216583b37fe815071a473fd49f65b01218e
Socheatdalin <dal88246@gmail.com>
2023-06-24 21:59:35 +0700

    update testing

--- Bookshop/vite.config.js
+++ Bookshop/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bash: '/Bookshop/Leorio.github.io',
+  bash: '/Leorio.github.io',
   plugins: [
     vue(),
   ],
