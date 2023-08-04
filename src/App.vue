579b3dd10d4923b2f82dc58a660c1f6dda1f4a8c
Socheatdalin <dal88246@gmail.com>
2023-08-04 19:27:21 +0700

    hosting

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import FooterView from '@/components/Footerview.vue'
+
 </script>
 
 <template>
 
         <RouterView />
-        <FooterView />
+        
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './app.css'
-// import store from './store'
+
 const app = createApp(App)
 
 app.use(router)
-// app.use(store)
+
 app.mount('#app')
 
-// createApp(App).use(router).use(store).mount('#app')
