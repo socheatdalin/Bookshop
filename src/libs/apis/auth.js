bf4f5f069e8b5907bb30643914ab3c6cf8ea7742
Socheatdalin <dal88246@gmail.com>
2023-08-05 21:52:46 +0700

    update1

--- src/libs/apis/auth.js
+++ src/libs/apis/auth.js
@@ -1,21 +1,21 @@
 var auth = {
-        async getMe(){
-                try{
-                        const res = await fetch('https://dalin-bookshop-api.onrender.com/auth/me',{
-                                method: 'GET',
-                                credentials: "include",
-                                header:{
-                                        "Content-type": "application/json",
-                                }
-                        });
-                        const result = await res.json();
-                        return result;  
-                }
-                 catch(err){
-                        console.log(err)
-                 }        
+        // async getMe(){
+        //         try{
+        //                 const res = await fetch('https://dalin-bookshop-api.onrender.com/auth/me',{
+        //                         method: 'GET',
+        //                         credentials: "include",
+        //                         header:{
+        //                                 "Content-type": "application/json",
+        //                         }
+        //                 });
+        //                 const result = await res.json();
+        //                 return result;  
+        //         }
+        //          catch(err){
+        //                 console.log(err)
+        //          }        
                
-        },
+        // },
         async logout(){
                 const res = await fetch('https://dalin-bookshop-api.onrender.com/auth/logout',{
                         method: "POST",
